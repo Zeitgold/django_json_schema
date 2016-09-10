0f3e4bf191bb141c96f75e14e3528737e046ed66
tom <tom@zeitgold.com>
2016-09-10 13:26:40 +0200

    added travis pypi credentials

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'tom-zeit/django_json_schema'
+GITHUB_REPO = 'Zeitgold/django_json_schema'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
